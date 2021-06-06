1bcce87db16397cbb28262952b528788fb389e1f
Safal Adhikari <mr.safaladh@gmail.com>
2021-06-06 14:55:17 +0545

    Updated name above message

--- messenger/src/Message.js
+++ messenger/src/Message.js
@@ -8,14 +8,13 @@ const Message = forwardRef(({ message, username }, ref) => {
 
   return (
     <div ref={ref} className={`message ${isUser && "message__user"}`}>
-      {/* <p style={{ margin: 0, left: 0 }}>
-        {!isUser && `${message.username}`}
-        {message.username}
-      </p> */}
+      <p className="user__name">
+        {!isUser && `${message.username || "anonymous"}`}
+      </p>
       <Card className={isUser ? "message__userCard" : "message__guestCard"}>
         <CardContent>
           <Typography color="white" variant="h5" component="h2">
-            {!isUser && `${message.username || "anonymous"}:`} {message.message}
+            {message.message}
           </Typography>
         </CardContent>
       </Card>
